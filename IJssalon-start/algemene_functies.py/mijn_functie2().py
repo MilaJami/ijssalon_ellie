3bda5787aae2f667e6776327cb7ee5965b868bfe
Jamila Schrijver <jamila.schrijver83@gmail.com>
2025-09-21 20:55:02 +0200

    toegevoegd opdracvht 8.12.2

---
+++ IJssalon-start/algemene_functies.py/mijn_functie2().py
@@ -0,0 +1,26 @@
+def mijn_functie_1(mijn_int1a, mijn_int1b, mijn_int1c, mijn_int1d, 
+                   mijn_int2a, mijn_int2b, mijn_int2c, mijn_int2d,
+                 mijn_int3a, mijn_int3b, mijn_int3c, mijn_int3d, 
+                 mijn_int4a, mijn_int4b, mijn_int4c, mijn_int4d):
+    return mijn_int1a + 3, mijn_int1b - 3, mijn_int1c * 3, mijn_int1d / 3,  mijn_int2a + 2, mijn_int2b - 2, mijn_int2c * 2, mijn_int2d / 2,mijn_int3a + 5, mijn_int3b - 5, mijn_int3c * 5, mijn_int3d / 5,mijn_int4a + 20, mijn_int4b - 10, mijn_int4c * 20, mijn_int4d / 20
+mijn_int1a = 12
+mijn_int1b = 12
+mijn_int1c = 12
+mijn_int1d = 12
+mijn_int2a = 12
+mijn_int2b = 12
+mijn_int2c = 12
+mijn_int2d = 12
+mijn_int3a = 10
+mijn_int3b = 10
+mijn_int3c = 10
+mijn_int3d = 10
+mijn_int4a = 100
+mijn_int4b = 100
+mijn_int4c = 100
+mijn_int4d = 100
+
+print(mijn_functie_1(mijn_int1a, mijn_int1b, mijn_int1c, mijn_int1d,
+                   mijn_int2a, mijn_int2b, mijn_int2c, mijn_int2d,
+                 mijn_int3a, mijn_int3b, mijn_int3c, mijn_int3d,
+                 mijn_int4a, mijn_int4b, mijn_int4c, mijn_int4d))
