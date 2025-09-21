987b3984674add65412d38c8871da8392cefd959
Jamila Schrijver <jamila.schrijver83@gmail.com>
2025-09-21 20:30:11 +0200

    opdracht toegevoegd 8.12.1

---
+++ IJssalon-start/algemene_functies.py/mijn_functie_1().py
@@ -0,0 +1,10 @@
+""""Hoe kan het dat deze functie Bruin kleurt?"""
+
+def mijn_functie_1(mijn_int1, mijn_int2, mijn_int3, mijn_int4):
+    return mijn_int1 * 2, mijn_int2 * 4, mijn_int3 * 10, mijn_int4 * 12
+mijn_int1 = 2
+mijn_int2 = 4
+mijn_int3 = 10
+mijn_int4 = 12
+
+print(mijn_functie_1(mijn_int1,mijn_int2,mijn_int3,mijn_int4)) 
